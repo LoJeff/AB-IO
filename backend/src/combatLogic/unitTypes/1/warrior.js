dbe794b9b0bd8521b9943db8c72853ab73ae1a94
LoJeff <jefflo2381@gmail.com>
2019-09-23 20:16:34 -0700

    Restructuring how unit types are saved

---
+++ backend/src/combatLogic/unitTypes/1/warrior.js
@@ -0,0 +1,12 @@
+class WARRIOR {
+    constructor() {
+        this.infoLvl = [
+            undefined,
+            {hp: 100, pDmg: 30, range: 1},
+            {hp: 200, pDmg: 50, range: 1},
+            {hp: 400, pDmg: 100, range: 1}        
+        ];
+    }
+}
+
+export default WARRIOR;
